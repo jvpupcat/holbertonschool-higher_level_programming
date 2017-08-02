43eea58f58d99fd7d4f0691cc857bb575b85837f
jvpupcat <vuong.joann@yahoo.com>
2017-08-02 03:42:00 +0000

    added 3rd print statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for a in range(0, 9):
     for b in range(0, 10):
-        if a != b and b > a and a != 8 and b != 9:
-            print("{}{}, ".format(a, b), end='')
-        if a == 8 and b == 9:
-            print("{}{}".format(a, b), end='\n')
+        if a != b and b > a:
+            print("{}{}".format(a, b), sep='', end='')
+            if a != 8 or b != 9:
+                print(", ", end='')
+print('')
