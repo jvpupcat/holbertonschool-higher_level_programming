ecf6e523c557053964d2ecdd6cd00da8b895f4f6
jvpupcat <vuong.joann@yahoo.com>
2017-08-02 22:56:19 +0000

    reduced to 1 print stmnt

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for z in range(122, 96, -1):
-    if z % 2 == 1:
-        print("{:s}".format(chr(z - 32)), end='')
-    else:
-        print("{:s}".format(chr(z)), end='')
+        print("{:s}".format(chr(z - 32) if z % 2 == 1 else chr(z)), end='')
