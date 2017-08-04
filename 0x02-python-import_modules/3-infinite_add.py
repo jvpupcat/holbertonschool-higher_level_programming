ee2d4e71d77f5e793a69f1925fea570ec0384314
jvpupcat <vuong.joann@yahoo.com>
2017-08-04 04:15:11 +0000

    first commit

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,6 @@ if __name__ == "__main__":
     import sys
     total = 0
 
-    if len(sys.argv) == 1:
-        print("0")
     for i in range(1, len(sys.argv)):
         total = total + int(sys.argv[i])
     print("{:d}".format(total))
