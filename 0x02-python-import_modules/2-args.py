27cce6317065a90e9d8d2bf4fb1ba2b280c95b3b
jvpupcat <vuong.joann@yahoo.com>
2017-08-04 03:30:31 +0000

    removed from arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) == 1:
-        print("0 arguments.")
+        print("0 argument.")
     if len(sys.argv) == 2:
         print("1 argument:")
     if len(sys.argv) > 2:
