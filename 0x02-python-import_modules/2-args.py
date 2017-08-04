e1a07f4e7c8d0240d1c6c7b44bee10c1b126d285
jvpupcat <vuong.joann@yahoo.com>
2017-08-04 03:27:56 +0000

    removed excess if statement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
-        if len(sys.argv[i]) == 2:
-            print("{:d}: {}".format(i, sys.argv[i]))
-        if len(sys.argv[i]) > 2:
-            print("{:d}: {}".format(i, sys.argv[i]))
+        print("{:d}: {}".format(i, sys.argv[i]))
