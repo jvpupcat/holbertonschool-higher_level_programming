195602c4878df593587d51c7e172e6ea0f4945df
jvpupcat <vuong.joann@yahoo.com>
2017-08-04 03:20:31 +0000

    changed if statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,10 @@ if __name__ == "__main__":
         print("0 arguments.")
     if len(sys.argv) == 2:
         print("1 argument:")
-        print("{:d}: {:s}".format(len(sys.argv) - 1, sys.argv[1]))
     if len(sys.argv) > 2:
         print("{:d} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        if len(sys.argv[i]) == 2:
+            print("{:d}: {}".format(i, sys.argv[i]))
+        if len(sys.argv[i]) > 2:
+            print("{:d}: {}".format(i, sys.argv[i]))
