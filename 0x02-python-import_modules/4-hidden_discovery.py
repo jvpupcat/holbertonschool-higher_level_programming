e3f9f2fd93798361b42de6f81adad059dbfc1349
jvpupcat <vuong.joann@yahoo.com>
2017-08-07 03:47:57 +0000

    removed sorted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    hidden = sorted(dir('hidden_4'))
+    hidden = dir('hidden_4')
     for i in range(0, len(hidden)):
         if hidden[i][:2] != "__":
             print("{}".format(hidden[i]))
