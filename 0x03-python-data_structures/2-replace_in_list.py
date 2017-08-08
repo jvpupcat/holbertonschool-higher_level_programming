ce0123da1d5d98c1a38bceeabc8aaf9eccbfe2af
jvpupcat <vuong.joann@yahoo.com>
2017-08-08 22:57:05 +0000

    fixed range

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for a in my_list:
-        if a == idx:
+    for a in range(0, len(my_list)):
+        if my_list[a] == idx + 1:
             my_list[a] = element
     return (my_list)
