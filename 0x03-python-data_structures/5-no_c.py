c23ba35e70e7905f6ccc1202051dc3feb6c9ad2d
jvpupcat <vuong.joann@yahoo.com>
2017-08-09 04:19:58 +0000

    used pass, created empty string variable

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string[:]
-    for c in range(0, len(new_string)):
-        if new_string[c] == chr(67):
-            new_string = new_string.strip('C')
-        elif new_string[c] == chr(99):
-            new_string = new_string.strip('c')
+    new_string = ''
+    for c in my_string:
+        if c == 'c':
+            pass
+        elif c == 'C':
+            pass
         else:
-            return (new_string)
+            new_string = new_string + c
+    return (new_string)
