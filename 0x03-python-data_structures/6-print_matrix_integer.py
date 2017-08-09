5abab303fe830e94165ea89967d5612bf31b7ecf
jvpupcat <vuong.joann@yahoo.com>
2017-08-09 05:37:24 +0000

    added :d

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in range(len(i)):
             if j == (len(i) - 1):
-                print("{}".format(i[j]), end='')
+                print("{:d}".format(i[j]), end='')
             else:
-                print("{} ".format(i[j]), end='')
+                print("{:d} ".format(i[j]), end='')
         print()
