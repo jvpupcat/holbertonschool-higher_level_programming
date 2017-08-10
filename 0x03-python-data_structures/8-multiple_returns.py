bfc24429ce2dfc1f79432446ca220580f0472b0d
jvpupcat <vuong.joann@yahoo.com>
2017-08-10 03:38:03 +0000

    removed for loop

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == 0:
+    if length == '':
         return None
     else:
-        for i in sentence:
-            return length, sentence[0]
+        return length, sentence[0]
