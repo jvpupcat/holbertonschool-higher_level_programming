54c2ce21373fd61fb47245a579c17874ae0fa6ca
jvpupcat <vuong.joann@yahoo.com>
2017-08-10 03:36:58 +0000

    replaced empty '' with 0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if length == '':
+    if length == 0:
         return None
     else:
         for i in sentence:
