26816e2a5c5b5210e603ba174b50ba5516de1e6f
jvpupcat <vuong.joann@yahoo.com>
2017-08-11 04:02:46 +0000

    changed if statement

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
-    if key == "" or key is None:
+    if my_dict == {}:
         my_dict[key] = value
     if key != value:
         my_dict[key] = value
