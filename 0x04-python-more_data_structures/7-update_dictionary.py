3e75b3a4c42bbdf93abbaa13041561c37db3eb0a
jvpupcat <vuong.joann@yahoo.com>
2017-08-11 03:58:25 +0000

    added if key doesn't exist

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(my_dict, key, value):
+    if key == "" or key is None:
+        my_dict[key] = value
     if key != value:
         my_dict[key] = value
     return my_dict
