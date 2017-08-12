293cdf3b9e85109492a06e48838e88836e8400ac
jvpupcat <vuong.joann@yahoo.com>
2017-08-12 21:29:34 +0000

    first commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    hidden = dir('hidden_4')
-    for i in range(0, len(hidden)):
-        if hidden[i][:2] != "__":
-            print("{}".format(hidden[i]))
+    import hidden_4
+    store_hid = dir(hidden_4)
+    for x in store_hid:
+        if x[:2] != '__':
+            print(x)
