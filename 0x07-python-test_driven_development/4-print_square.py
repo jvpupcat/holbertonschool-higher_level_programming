c01cde747c052626fe08d4274073f7d3293adbfb
jvpupcat <vuong.joann@yahoo.com>
2017-08-26 06:42:30 +0000

    added 2 if statements

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_square(size):
-    for x in range(0, size):
-        for y in range(0, size):
-            print("#", end="")
-        print("")
+    if type(size) is int:
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        for x in range(0, size):
+            for y in range(0, size):
+                print("#", end="")
+            print("")
+    elif type(size) is not int:
+        raise TypeError("size must be an integer")
