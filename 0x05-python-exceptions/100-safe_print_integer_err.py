04b05509dc6fd43788f893f37fc570f2df93b3ba
jvpupcat <vuong.joann@yahoo.com>
2017-08-27 05:47:22 +0000

    removed type()

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,13 +2,12 @@
 def safe_print_integer_err(value):
     import sys
 
-    if type(value) is int:
-        try:
-            print("{:d}".format(value))
-            return True
-        except TypeError as te:
-            print("Exception: {}".format(te), file=stderr)
-            return False
-        except ValueError as ve:
-            print("Exception: {}".format(ve), file=stderr)
-            return False
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as te:
+        print("Exception: {}".format(te), file=sys.stderr)
+        return False
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=sys.stderr)
+        return False
