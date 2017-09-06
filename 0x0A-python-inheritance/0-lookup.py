30dfcb805d05cf15fb580a90413c8757219eb116
jvpupcat <vuong.joann@yahoo.com>
2017-09-06 23:45:09 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that returns the list of available attributes and methods
+of an object.
+
+Returns a list of objects
+"""
+
+
+def lookup(obj):
+    return dir(obj)
