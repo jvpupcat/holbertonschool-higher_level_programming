f1b81a4569d9c7709fabb54c3baf99cf220f1032
jvpupcat <vuong.joann@yahoo.com>
2017-09-08 00:47:40 +0000

    first commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file(UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding='utf-8') as a_file:
+        for x in a_file:
+            print(x, end="")
