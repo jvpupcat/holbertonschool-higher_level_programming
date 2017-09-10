7f7289ded3325c50ff2eebc9654698e89712a647
jvpupcat <vuong.joann@yahoo.com>
2017-09-10 18:09:06 +0000

    first commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""first class Base"""
+
+
+class Base:
+    """private class attribute"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            """assign the public instance attribute id to this argument value"""
+            self.id = id
+        else:
+            """increment and assign value to public instance attribute id"""
+            Base.__nb_objects += 1
+            self.id = self.__nb_objects
