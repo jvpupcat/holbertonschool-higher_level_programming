d1c83136ac396b5f85661efdf97160fc032d47cb
jvpupcat <vuong.joann@yahoo.com>
2017-09-11 17:35:49 +0000

    added 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -64,8 +64,9 @@ class Rectangle(Base):
         return int(self.__width) * int(self.__height)
 
     def display(self):
+        print('\n' * self.__y, end="")
         for i in range(self.__height):
-            print('#' * self.__width)
+            print((' ' * self.__x) + ('#' * self.__width))
 
     def __str__(self):
         a = self.id
