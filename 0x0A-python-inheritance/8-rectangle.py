b4708343587769e749f0aa88973afc2f0c6c040f
jvpupcat <vuong.joann@yahoo.com>
2017-09-11 03:16:23 +0000

    added extra space

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,6 +3,7 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         if BaseGeometry.integer_validator(self, "width", width):
