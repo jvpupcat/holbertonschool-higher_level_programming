b76cc854294bc18c66f16c15e46c70f684f73b2d
jvpupcat <vuong.joann@yahoo.com>
2017-09-11 05:29:30 +0000

    first commit

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
