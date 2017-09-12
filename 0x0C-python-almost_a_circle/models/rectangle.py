dc46600d7bfe2f3eef0cc2cbf7d3ace70743b68b
jvpupcat <vuong.joann@yahoo.com>
2017-09-12 21:58:32 +0000

    initialized private instance attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,10 +6,10 @@ from models.base import Base
 class Rectangle(Base):
     """initialize rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
         super(Rectangle, self).__init__(id)
     
     @property
