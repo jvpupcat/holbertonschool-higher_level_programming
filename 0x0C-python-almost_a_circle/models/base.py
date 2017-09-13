16b4519e7c18a0efbe05f158af2e6fbf486d79fd
jvpupcat <vuong.joann@yahoo.com>
2017-09-13 20:42:09 +0000

    added static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """dictionary to JSON string"""
         if list_dictionaries is None:
