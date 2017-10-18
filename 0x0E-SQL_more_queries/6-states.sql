17fa660e2c593f2bc009b326bb5756db499e4254
jvpupcat <vuong.joann@yahoo.com>
2017-10-18 01:42:31 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- script that creates the database hbtn_0d_usa and
+-- the table states
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS state (id INT PRIMARY KEY NOT NULL, name VARCHAR(256) NOT NULL);
