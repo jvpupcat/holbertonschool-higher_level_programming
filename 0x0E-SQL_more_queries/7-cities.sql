91a27093536b45d3a6c60ff97852e300b7c1265e
jvpupcat <vuong.joann@yahoo.com>
2017-10-18 03:01:13 +0000

    added increment

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- table cities
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_GENERATED PRIMARY KEY NOT NULL, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.cities(id), name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.cities(id), name VARCHAR(256) NOT NULL);
