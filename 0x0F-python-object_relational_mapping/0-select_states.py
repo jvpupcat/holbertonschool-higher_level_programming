011680743a35b2c34a4ee2dae881a801803ad722
jvpupcat <vuong.joann@yahoo.com>
2017-11-09 08:16:50 +0000

    fixed syntax error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1]
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY states.id ASC")
