d6c35882cc7f6777284616e83857b4e200316328
jvpupcat <vuong.joann@yahoo.com>
2017-11-09 09:14:31 +0000

    fixed pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""script that pulls in argv4"""
 
 if __name__ == "__main__":
     import MySQLdb
     import sys
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-				passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     arg4 = sys.argv[4]
     cur.execute("SELECT  id, name\
