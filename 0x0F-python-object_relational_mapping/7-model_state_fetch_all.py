acb373f10ba04cb3d2620fa8c0479cffebb543b1
jvpupcat <vuong.joann@yahoo.com>
2017-11-09 20:59:15 +0000

    fixed pep8 issues

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,16 +4,18 @@
 if __name__ == "__main__":
     import sqlalchemy
     from sqlalchemy.orm import sessionmaker
-    import model_state
+    from model_state import Base, State
     import sys
 
-    engine = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'\
-                                      .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    sub_lochost = 'mysql+mysqldb://{}:{}@localhost: 3306/{}'
+    engine = sqlalchemy.create_engine(sub_lochost.format
+                                      (sys.argv[1], sys.argv[2],
+                                       sys.argv[3]))
 
-    model_state.Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(model_state.State).order_by(model_state.State.id).all():
+    for state in session.query(State).order_by(State.id).all():
         print("{}: {}".format(state.id, state.name))
     session.close()
