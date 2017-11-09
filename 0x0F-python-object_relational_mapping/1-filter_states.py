2b34b355817323c8fda20d1a810cab9477f67cac
jvpupcat <vuong.joann@yahoo.com>
2017-11-09 06:14:09 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+				passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name\
+                FROM states\
+                WHERE name REGEXP '^N'\
+                ORDER BY states.id ASC")
+    query = cur.fetchall()
+    for row in query:
+        print(row)
+    cur.close()
+    db.close()
