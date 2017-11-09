5de97c8d0c5d8ec5de48a12ba93de2c40e6484fd
jvpupcat <vuong.joann@yahoo.com>
2017-11-09 11:54:07 +0000

    fixed pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import MySQLdb
     import sys
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-				passwd=sys.argv[2], db=sys.argv[3])
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     arg4 = sys.argv[4]
     cur.execute("SELECT  id, name\
