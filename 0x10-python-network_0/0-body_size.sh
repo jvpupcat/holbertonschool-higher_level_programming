9c5841ab4bd0b5c2e1dd50aad9bb85260054bb3a
jvpupcat <vuong.joann@yahoo.com>
2017-11-22 00:12:23 +0000

    added quotation marks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,4 +2,4 @@
 # script that takes in URL, sends request to URL, and displays
 # size of the body of the response
 
-curl -sI $1 | grep Content-Length | awk '{print $2}'
+curl -sI "$1" | grep Content-Length | awk '{print $2}'
