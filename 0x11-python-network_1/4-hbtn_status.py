4ce03d10d7ed56bc2e079eba98242d1d40e39293
jvpupcat <vuong.joann@yahoo.com>
2017-11-28 03:34:32 +0000

    first commit

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+python script that fetches https://intranet.hbtn.io/status
+'''
+
+if __name__ == "__main__":
+    import requests
+    url = 'https://intranet.hbtn.io/status'
+    req = requests.get(url)
+    display = type(req.text)
+    print("Body response:")
+    print("\t- type:", display)
+    print("\t- utf8 content:", req.text)
