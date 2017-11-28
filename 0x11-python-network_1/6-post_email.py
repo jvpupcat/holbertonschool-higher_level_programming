e773c47a7af1da72a113272b8537f2d6c2abb175
jvpupcat <vuong.joann@yahoo.com>
2017-11-28 05:20:37 +0000

    added .content

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-'''
-Write a Python script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as
-a parameter, and displays the body of the response
-(decoded in utf-8)
-'''
+"""
+Write a Python script that takes in a URL and an email
+address, sends a POST request to the passed URL with the email
+as a parameter, and finally displays the body of the response.
+"""
 
 if __name__ == "__main__":
     import requests
@@ -13,5 +12,5 @@ if __name__ == "__main__":
     email = {}
     email['email'] = sys.argv[2]
     req = requests.post(url, email)
-    decode = req.decode("utf-8")
+    decode = req.content.decode("utf-8")
     print(decode)
