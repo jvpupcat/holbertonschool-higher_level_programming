5d86f339979851a430777ede1a1309606abce39c
jvpupcat <vuong.joann@yahoo.com>
2017-11-28 05:44:27 +0000

    first commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL
+and displays the body of the response.
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    url = sys.argv[1]
+    req = requests.get(url)
+    status = req.status_code
+    if status >= 400:
+        print("Error code:", status)
+    else:
+        print(req.text)
