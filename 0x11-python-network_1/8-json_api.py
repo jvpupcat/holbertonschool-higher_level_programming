e56e2c91ebcf2083b1d9c84234995a98e040b3c5
jvpupcat <vuong.joann@yahoo.com>
2017-11-28 21:46:24 +0000

    first commit

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,17 +5,20 @@ POST request to http://0.0.0.0:5000/search_user with the
 letter as a parameter.
 """
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     import sys
     import requests
 
-    if sys.argv[1] is "":
-        q = ""
-    else:
-        q = sys.argv[1]
-    req = request.post('http://0.0.0.0:5000/search_user', q)
-    results = req.json
-    if results is None:
-        print("No result")
-    else:
-        print("[{}] {}".format(results.id, results.name))
+    try:
+        value = sys.argv[1]
+    except:
+        value = ""
+    req = requests.post('http://172.31.54.208:36717/search_user', data = {'q': value})
+    try:
+        results = req.json()
+        if not results:
+            print("No result")
+        else:
+            print("[{}] {}".format(results.get('id'), results.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
