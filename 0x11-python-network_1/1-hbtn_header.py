6b32fb8b29436ca927414c5eaa89f2cde71f67ed
jvpupcat <vuong.joann@yahoo.com>
2017-11-28 00:33:21 +0000

    added name == main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,9 +5,10 @@ URL and displays the value of the X-Request-Id variable found
 in the header of the response.
 '''
 
-import urllib.request
-import sys
-url_arg = urllib.request.Request(sys.argv[1])
-with urllib.request.urlopen(url_arg) as response:
-    display = response.getheader('X-Request-Id')
-    print(display)
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url_arg = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(url_arg) as response:
+        display = response.getheader('X-Request-Id')
+        print(display)
