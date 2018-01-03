66347fa69921badb5505ece8073334c763da3cb4
jvpupcat <130@holbertonschool.com>
2018-01-03 15:23:51 +0000

    added parseInt

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let a = process.argv[2];
-let b = process.argv[3];
-function add(a, b) {
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+function add (a, b) {
   if (!a || !b) {
     return NaN;
   } else {
-      return a + b;
+    return a + b;
   }
 }
-console.log(a + b);
+console.log(add(a, b));
