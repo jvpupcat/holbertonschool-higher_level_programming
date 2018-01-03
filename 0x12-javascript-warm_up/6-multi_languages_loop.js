07b7a95bd2e772c2ce7f0cbf483810072d3167c4
jvpupcat <130@holbertonschool.com>
2018-01-03 14:04:34 +0000

    fixed <= to <

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 let lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-for (i = 0; i <= lines.length; i++) {
+for (i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
