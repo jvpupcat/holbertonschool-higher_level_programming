c30e8e6ad67d1b9e897d60a789210728a19dbc50
jvpupcat <130@holbertonschool.com>
2018-01-03 13:37:17 +0000

    first commit

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (!process.argv[2]) {
+  console.log('Not a number');
+} else if (!isNaN(process.argv[2])) {
+  console.log('My number: ' + process.argv[2]);
+} else {
+  console.log('Not a number');
+}
