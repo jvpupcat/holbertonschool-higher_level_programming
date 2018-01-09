a9534a617c555798973f14bd19bf29e68cd80f99
jvpupcat <130@holbertonschool.com>
2018-01-09 23:52:22 +0000

    reduced for loops

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 const arg = process.argv[2];
+const characterID = 'http://swapi.co/api/people/18/';
 request(arg, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let results = (JSON.parse(body).results);
-    for (let i = 0; results != NULL; i++) {
-      if (results[i] === 'characters') {
-        for (let j = 0; j < characters; j++) {
-          if (characters[j] === '1' && characters[j + 1] === '8') {
-            console.log(results.character)
-          }
-        }
+    let resultsize = JSON.parse(body).results.length;
+    let count = 0;
+    for (let i = 0; i < resultsize; i++) {
+      if (JSON.parse(body).results[i].characters === characterID) {
+        count++;
       }
     }
+    console.log(count);
   }
 });
