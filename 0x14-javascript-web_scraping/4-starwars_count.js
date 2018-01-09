067c259055cb44a70d5bd74be26ccc2d1db13b76
jvpupcat <130@holbertonschool.com>
2018-01-09 23:44:12 +0000

    added loops

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,8 +2,18 @@
 const request = require('request');
 const arg = process.argv[2];
 request(arg, function (error, response, body) {
-  if (error) console.log(error);
-  else {
-    console.log(JSON.parse(body).endsWith(18));
+  if (error) {
+    console.log(error);
+  } else {
+    let results = (JSON.parse(body).results);
+    for (let i = 0; results != NULL; i++) {
+      if (results[i] === 'characters') {
+        for (let j = 0; j < characters; j++) {
+          if (characters[j] === '1' && characters[j + 1] === '8') {
+            console.log(results.character)
+          }
+        }
+      }
+    }
   }
 });
