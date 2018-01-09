000687b25a0749e1b22509d6743d9b12a972d422
jvpupcat <130@holbertonschool.com>
2018-01-09 13:33:43 +0000

    semistandard --fix

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,8 +3,8 @@ module.exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (let i = 0; i <= list; i++) {
     if (searchElement === list[i]) {
-      count += 1;
+      count++;
     }
-  };
+  }
   return count;
-}
+};
