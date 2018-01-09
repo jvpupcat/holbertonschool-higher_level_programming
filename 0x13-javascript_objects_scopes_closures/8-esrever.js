020eb8dcedf39e97aed7077ad9af65992fcd27d8
jvpupcat <130@holbertonschool.com>
2018-01-09 14:09:32 +0000

    used length

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports.esrever = function (list) {
   let reverse = new Array;
-    for (let i = 0; i <= list; i--) {
+    for (let i = list.length - 1; i >= 0; i--) {
       reverse.push(list[i])
     }
   return reverse;
