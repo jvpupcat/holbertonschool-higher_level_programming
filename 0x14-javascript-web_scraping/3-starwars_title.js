b09c7858f44674153c8aa2ad677920ade6597d48
jvpupcat <130@holbertonschool.com>
2018-01-09 23:20:50 +0000

    fixed URL issue

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const request = require('request');
 const arg = process.argv[2];
 const StarWarsURL = 'http://swapi.co/api/films/' + arg;
-request(arg, function (error, response, body) {
+request(StarWarsURL, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
