79e5b6bf68eaffb1a92a8e33bee79263a0960983
jvpupcat <130@holbertonschool.com>
2018-01-09 14:12:44 +0000

    removed new Array()

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 module.exports.esrever = function (list) {
-  let reverse = new Array;
-    for (let i = list.length - 1; i >= 0; i--) {
-      reverse.push(list[i])
-    }
+  let reverse = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reverse.push(list[i]);
+  }
   return reverse;
-}
+};
