78a65ff654d82d22e9c96f437204c9f679d0e3da
jvpupcat <130@holbertonschool.com>
2018-01-09 23:14:22 +0000

    fixed Error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const arg = process.argv[2]
+const arg = process.argv[3]
 const StarWarsURL = 'http://swapi.co/api/films/' + arg
 request(arg, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body).title)
+    console.log(JSON.parse(body)['title']);
   }
 });
