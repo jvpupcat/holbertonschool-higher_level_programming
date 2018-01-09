d91bfbfb863e444b7675d3800ce0aee9bb6f9987
jvpupcat <130@holbertonschool.com>
2018-01-09 23:16:00 +0000

    fixed syntax errors

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const arg = process.argv[3]
-const StarWarsURL = 'http://swapi.co/api/films/' + arg
+const arg = process.argv[2];
+const StarWarsURL = 'http://swapi.co/api/films/' + arg;
 request(arg, function (error, response, body) {
   if (error) {
     console.log(error);
