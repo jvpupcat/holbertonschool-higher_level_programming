93e3a4e1897cb7d13dc5c914d1d3caf565c54cff
jvpupcat <130@holbertonschool.com>
2018-01-10 00:09:31 +0000

    semistandard --fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fs = require('fs');
 const URLarg = process.argv[2];
 const filearg = process.argv[3];
-request(URLarg, function(error, response, body){
+request(URLarg, function (error, response, body) {
   if (error) console.log(error);
   else {
     fs.writeFile(filearg, body, 'utf8', function (error) {
