355627383e93d141c6e370e32dfeb5690361f016
jvpupcat <130@holbertonschool.com>
2018-01-14 15:46:08 +0000

    removed {key, value}

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $.getJSON('https://swapi.co/api/people/5/?format=json',
-            {key, value}), function(data){
+            function(data){
       $('#character').html(data.name);
-    }
+    });
 });
